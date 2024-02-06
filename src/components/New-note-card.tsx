01b19a56e7fca978fc827010c4a0d9c754d16b35
Pedro Grunwald <pedro.grunwald.com>
2024-02-06 14:10:23 -0300

    feat: add New-Note Component

---
+++ src/components/New-note-card.tsx
@@ -0,0 +1,10 @@
+export function NewNoteCard() {
+  return (
+    <div className="roundend-md space-y-3 p-5 overflow-hidden relative bg-slate-800 ">
+      <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
+      <p className="text-sm leading-6 text-slate-400">
+        Grave uma nota em audio que será convertido em texto
+      </p>
+    </div>
+  );
+}

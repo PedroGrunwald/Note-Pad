8873cfb5701a93ade8820d05a589a4414795d56f
Pedro Grunwald <pedro.grunwald.com>
2024-02-13 16:56:58 -0300

    feat: add note

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import logo from "./assets/Logo.svg";
 import { NewNoteCard } from "./components/New-note-card";
 import { Notecard } from "./components/Note-card";
 
+const note = {
+  date: new Date(),
+  content: "Hello Word",
+};
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -18,14 +23,9 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-       
-       <NewNoteCard/>
+        <NewNoteCard />
 
-        <Notecard/>
-        <Notecard/>
-        <Notecard/>
-        
-
+        <Notecard note={note} />
       </div>
     </div>
   );
